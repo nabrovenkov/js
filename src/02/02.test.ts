247c3cfb57659aa245800392a72ef38ad7470473
Alexey Nabrovenkov <nabrovenkov@gmail.com>
2024-01-30 17:04:03 +0300

    1

---
+++ src/02/02.test.ts
@@ -0,0 +1,16 @@
+import { CityType } from "./02_02";
+
+let city: CityType;
+
+beforeEach(() => {
+  city = {
+    title: 'New York',
+    houses: [{}, {}, {}],
+    governmentBuildings: [],
+    citizensNumber: 100000
+  }
+})
+
+test('test city should contains 3 houses', ( => {
+  
+}))
---
+++ src/02/02.ts
@@ -0,0 +1,52 @@
+type LocalCityType = {
+  title: string
+  countryTitle: string
+}
+
+type AddressType = {
+  streetTitle: string
+  city: CityType
+}
+
+type TechType = {
+  id: number
+  title: string
+}
+
+type StudentType = {
+  id: number
+  name: string
+  age: number
+  isActive: boolean
+  address: AddressType
+  technologies: Array<TechType>
+}
+
+const student: StudentType = {
+  id: 1,
+  name: 'Alex',
+  age: 32,
+  isActive: false,
+  address: {
+    streetTitle: 'Surganova 2',
+    city: {
+      title: 'Minsk',
+      countryTitle: 'Belarus'
+    }
+  },
+  technologies: [
+    {
+      id: 1,
+      title: 'HTML'
+    },
+    {
+      id: 2,
+      title: 'CSS'
+    },
+    {
+      id: 3,
+      title: 'REACT'
+    }
+  ]
+}
+
