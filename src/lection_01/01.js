6a53baf008f36af65d76dfa161355352d0d07eda
Alexey Nabrovenkov <nabrovenkov@gmail.com>
2024-02-01 18:01:49 +0300

    1

---
+++ src/lection_01/01.js
@@ -0,0 +1,10 @@
+export const person = {
+  name: 'Kirill',
+  age: 24,
+  address: {
+    country: 'Poland',
+    city: 'Warsaw'
+  },
+};
+
+console.log(person)
---
+++ src/lection_01/01.test.ts
@@ -0,0 +1,5 @@
+import person from '../01-tests/01'
+
+
+const sdf = person
+console.log(sdf) 
